7a18e324f6dd1ce57d808707062b18d7560890a3
Elizabeth <mwendemuthusi6@gmail.com>
2023-02-08 17:21:39 +0300

    Completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+class Book
+attr_reader :title
+attr_reader :author, :page_count, :genre
+  def initialize(title)
+    @title = title
+  end
+  def  author=(author_name)
+    @author = author_name
+  end
+  def  page_count=(page_count_number)
+    @page_count = page_count_number
+  end
+  def  genre=(genre_category)
+    @genre = genre_category
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
+
+b1= Book.new("And Then There Were None")
+b1.page_count= 200
+puts b1.page_count
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,31 @@
 # Make your shoe class here!
+class Shoe
+attr_reader :brand
+attr_reader :color, :size, :material, :condition
+  def initialize(brand)
+    @brand = brand
+  end
+  def  color=(color_of_shoe)
+    @color = color_of_shoe
+  end
+  def  size=(size_number)
+    @size = size_number
+  end
+  def  material=(material_used)
+    @material = material_used
+  end
+  def  condition=(condition_state)
+    @condition = condition_state
+  end
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition ="new"
+  end
+end
+
+s1= Shoe.new("Adidas")
+s1.condition= "old"
+
+puts s1.cobble
+
+
